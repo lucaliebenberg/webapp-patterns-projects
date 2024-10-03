df042e86062fd863604956b2b3d6b524f7873797
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-10-03 20:53:44 +0200

    chore: updates

--- CoffeeMasters/initial/services/Router.js
+++ CoffeeMasters/initial/services/Router.js
@@ -14,7 +14,7 @@ const Router = {
         // Process initial URL   
         Router.go(location.pathname);
     },    
-    go: (route, addToHistory=true) => {
+    go: async (route, addToHistory=true) => {
         if (addToHistory) {
             history.pushState({ route }, '', route);
         }
@@ -24,6 +24,7 @@ const Router = {
                 pageElement = document.createElement("menu-page");
                 break;
             case "/order":
+                await import("import OrderPage from './components/OrderPage.js'")
                 pageElement = document.createElement("order-page");
                 break;
             default:
@@ -34,15 +35,17 @@ const Router = {
                 break;   
         }
         if (pageElement) {
-            // get current page element            
-            let currentPage = document.querySelector("main").firstElementChild; 
-            if (currentPage) {
-                currentPage.remove();
-                document.querySelector("main").appendChild(pageElement);
-            } else {
-                document.querySelector("main").appendChild(pageElement);
+            function changePage() {
+                // get current page element            
+                let currentPage = document.querySelector("main").firstElementChild; 
+                if (currentPage) {
+                    currentPage.remove();
+                    document.querySelector("main").appendChild(pageElement);
+                } else {
+                    document.querySelector("main").appendChild(pageElement);
+                }
             }
-
+            document.startViewTransition(() => changePage());
         }
 
         window.scrollX = 0;
